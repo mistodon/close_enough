d5c927869aa838dc9251d564f685fdf497b5ceba
Vi <1149443+mistodon@users.noreply.github.com>
2017-03-06 13:39:20 +0000

    Allow restricting results to files or directories

---
+++ README.md
@@ -0,0 +1,9 @@
+close_enough
+===
+
+To do
+---
+
+1.  Allow recursing through directories
+2.  Cleaner input reading
+3.  Replace panics with elegant error reporting and exit code
--- src/ce.rs
+++ src/ce.rs
@@ -29,22 +29,58 @@ fn ce_app<'a, 'b>() -> App<'a, 'b>
             .multiple(true)
         )
         .arg(
-            Arg::with_name("cwd")
-            .long("--cwd")
-            .help("Use current working directory contents as inputs")
-            .conflicts_with("inputs")
-        )
-        .arg(
             Arg::with_name("sep")
             .long("--sep")
             .help("The seperator to join the results with;\nDefaults to newline")
             .takes_value(true)
             .default_value("\n")
         )
+        .arg(
+            Arg::with_name("cwd")
+            .long("--cwd")
+            .help("Use current working directory contents as inputs")
+            .conflicts_with("inputs")
+        )
+        .arg(
+            Arg::with_name("files_only")
+            .short("-f")
+            .help("Used with --cwd: only allow files in results")
+            .requires("cwd")
+            .conflicts_with("dirs_only")
+        )
+        .arg(
+            Arg::with_name("dirs_only")
+            .short("-d")
+            .help("Used with --cwd: only allow directories in results")
+            .requires("cwd")
+            .conflicts_with("files_only")
+        )
         .after_help(
 r#"Fuzzy-search a list of inputs with one or more query strings.
 The closest match to each query string is returned on its own line.
 If no inputs are provided, inputs are read from stdin."#)
+}
+
+#[derive(Copy, Clone)]
+enum CwdSearchStrategy
+{
+    FilesOnly,
+    DirectoriesOnly,
+    Anything
+}
+
+impl CwdSearchStrategy 
+{
+    fn create(cwd: bool, files_only: bool, dirs_only: bool) -> Option<CwdSearchStrategy>
+    {
+        match (cwd, files_only, dirs_only)
+        {
+            (true, true, _) => Some(CwdSearchStrategy::FilesOnly),
+            (true, _, true) => Some(CwdSearchStrategy::DirectoriesOnly),
+            (true, _, _) => Some(CwdSearchStrategy::Anything),
+            _ => None
+        }
+    }
 }
 
 
@@ -54,7 +90,9 @@ fn main()
 
     let queries = args.values_of("query").expect("Expected query argument");
     let separator = args.value_of("sep").expect("ce: error: could not find separator");
-    let input_lines = fetch_input_lines(args.values_of("inputs"), args.is_present("cwd"));
+    let cwd_search_strategy = CwdSearchStrategy::create(args.is_present("cwd"), args.is_present("files_only"), args.is_present("dirs_only"));
+
+    let input_lines = fetch_input_lines(args.values_of("inputs"), cwd_search_strategy);
 
     input_lines.get(0).expect("ce: error: no valid inputs");
 
@@ -70,27 +108,33 @@ fn main()
 }
 
 
-fn fetch_input_lines<'a, I>(input_args: Option<I>, using_cwd: bool) -> Vec<Cow<'a, str>>
+fn fetch_input_lines<'a, I>(input_args: Option<I>, cwd_search_strategy: Option<CwdSearchStrategy>) -> Vec<Cow<'a, str>>
     where I: Iterator<Item=&'a str>
 {
-    match (input_args, using_cwd)
+    match (input_args, cwd_search_strategy)
     {
         (Some(inputs), _) => inputs.map(|s| Cow::Borrowed(s)).collect(),
-        (None, true) => list_cwd(),
-        (None, false) => read_stdin()
+        (None, None) => read_stdin(),
+        (None, Some(strategy)) => list_cwd(strategy)
     }
 }
 
-fn list_cwd<'a>() -> Vec<Cow<'a, str>>
+fn list_cwd<'a>(strategy: CwdSearchStrategy) -> Vec<Cow<'a, str>>
 {
     let here = env::current_dir().expect("ce: error: failed to identify current directory");
     let contents = fs::read_dir(&here).expect("ce: error: failed to read current directory");
 
-    contents.map(|entry| Cow::Owned(
-        entry.expect("ce: error: failed to read directory entry")
-        .file_name()
-        .into_string()
-        .expect("ce: error: failed to read directory entry"))
+    contents.filter_map(move |entry|
+        {
+            let entry = entry.expect("ce: error: failed to read directory entry");
+            let entry = match strategy
+            {
+                CwdSearchStrategy::FilesOnly => if entry.file_type().expect("ce: error: failed to read file type").is_file() { Some(entry) } else { None },
+                CwdSearchStrategy::DirectoriesOnly => if entry.file_type().expect("ce: error: failed to read file type").is_dir() { Some(entry) } else { None },
+                _ => Some(entry)
+            };
+            entry.map(|entry| Cow::Owned(entry.file_name().into_string().expect("ce: error: failed to read directory entry")))
+        }
     ).collect()
 }
 
