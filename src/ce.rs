908d071bc545a4c2f32c176383be0d262c1d1577
Vi <1149443+mistodon@users.noreply.github.com>
2017-03-06 13:09:38 +0000

    Allow multiple queries/result and --cwd input

--- src/ce.rs
+++ src/ce.rs
@@ -1,6 +1,9 @@
 extern crate close_enough;
 extern crate clap;
 
+use std::borrow::Cow;
+use std::env;
+use std::fs;
 use std::io::{self, Read, Write};
 use clap::{App, Arg};
 
@@ -13,46 +16,80 @@ fn main()
         .about("Fuzzy-search the input and return the closest match")
         .arg(
             Arg::with_name("query")
-            .help("the string to search for")
+            .help("The string or strings to search for;\nIf multiple strings are given, the closest match of each is returned")
+            .multiple(true)
             .required(true)
         )
         .arg(
             Arg::with_name("inputs")
-            .help("lines of input to search")
+            .long("--inputs")
+            .short("-i")
+            .help("Lines of input to search")
+            .takes_value(true)
             .multiple(true)
         )
-        .after_help("Longer explaination to appear after the options when \
-                     displaying the help information from --help or -h")
+        .arg(
+            Arg::with_name("cwd")
+            .long("--cwd")
+            .help("Use current working directory contents as inputs")
+            .conflicts_with("inputs")
+        )
+        .arg(
+            Arg::with_name("sep")
+            .long("--sep")
+            .help("The seperator to join the results with;\nDefaults to newline")
+            .takes_value(true)
+            .default_value("\n")
+        )
+        .after_help(
+r#"Fuzzy-search a list of inputs with one or more query strings.
+The closest match to each query string is returned on its own line.
+If no inputs are provided, inputs are read from stdin."#)
         .get_matches();
 
-    let query = args.value_of("query").expect("Expected query argument");
-    let maybe_inputs = args.values_of("inputs");
+    let queries = args.values_of("query").expect("Expected query argument");
 
-    let result = match maybe_inputs
+    let input_lines = fetch_input_lines(args.values_of("inputs"), args.is_present("cwd"));
+    let inputs: Vec<&str> = input_lines.iter().map(|s| s.as_ref()).collect();
+    let separator = args.value_of("sep").expect("ce: error: could not find separator");
+
+    let output: Vec<&str> = queries.map(
+        |q| close_enough::closest_enough(&inputs, q).expect("ce: error: query failed to match any inputs")
+    ).collect();
+
+    let output = &output.join(separator);
+
+    io::stdout().write(&output.as_bytes()).expect("ce: error: failed to write results");
+}
+
+fn fetch_input_lines<'a, I>(input_args: Option<I>, using_cwd: bool) -> Vec<Cow<'a, str>>
+    where I: Iterator<Item=&'a str>
+{
+    match (input_args, using_cwd)
     {
-        Some(inputs) => ce_over_inputs(inputs.map(|s| s.as_ref()).collect(), query),
-        None => ce_over_stdin(query)
-    };
-
-    let output = result.expect("ce: error: could not match any inputs");
-    io::stdout().write(output.as_bytes()).expect("ce: error: failed to write result");
+        (Some(inputs), _) => inputs.map(|s| Cow::Borrowed(s)).collect(),
+        (None, true) => list_cwd(),
+        (None, false) => read_stdin()
+    }
 }
 
-fn ce_over_inputs(inputs: Vec<&str>, query: &str) -> Option<String>
+fn list_cwd<'a>() -> Vec<Cow<'a, str>>
 {
-    close_enough::closest_enough(&inputs, query).map(|s| s.to_owned())
+    let here = env::current_dir().expect("ce: error: failed to identify current directory");
+    let contents = fs::read_dir(&here).expect("ce: error: failed to read current directory");
+
+    contents.map(|entry| Cow::Owned(
+        entry.expect("ce: error: failed to read directory entry")
+        .file_name()
+        .into_string()
+        .expect("ce: error: failed to read directory entry"))
+    ).collect()
 }
 
-fn ce_over_stdin(query: &str) -> Option<String>
+fn read_stdin<'a>() -> Vec<Cow<'a, str>>
 {
-    let mut stdin = io::stdin();
-    let input = {
-        let mut s = String::new();
-        stdin.read_to_string(&mut s).expect("ce: error: failed to read from stdin");
-        s
-    };
+    let mut s = String::new();
+    io::stdin().read_to_string(&mut s).expect("ce: error: failed to read from stdin");
 
-    let lines: Vec<&str> = input.lines().collect();
-
-    close_enough::closest_enough(&lines, query).map(|s| s.to_owned())
+    s.lines().map(|s| Cow::Owned(s.to_owned())).collect()
 }
