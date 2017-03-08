2120240f9ac4be7f3bff44c5bc98ca2ca6b04964
Paul Harris <paul.harris@eegeo.com>
2017-03-08 11:42:03 +0000

    Fix ce script to work with directories that have spaces in their names

--- src/scripts/ce.sh
+++ src/scripts/ce.sh
@@ -1,7 +1,7 @@
 function ce()
 {
     if [ "$#" -gt 0 ]; then
-        local dest=$(cle -ce $@)
+        local dest=$(cle -ce "$@")
         if [ $? -eq 0 ]; then
             cd "${dest}"
         fi
