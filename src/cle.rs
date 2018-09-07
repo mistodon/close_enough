d65d78c727a70a9ea8e5d4bcc5493334537415f3
Vi <1149443+mistodon@users.noreply.github.com>
2018-09-07 17:27:18 +0100

    Give crate a make-over

--- src/cle.rs
+++ src/cle.rs
@@ -1,207 +1,192 @@
-extern crate close_enough;
 extern crate clap;
+extern crate close_enough;
 
 use clap::App;
 
-
-fn cle_app<'a, 'b>() -> App<'a, 'b>
-{
-    use clap::{Arg, AppSettings, SubCommand};
+fn cle_app<'a, 'b>() -> App<'a, 'b> {
+    use clap::{AppSettings, Arg, SubCommand};
 
     App::new("cle")
         .author("Pirh, ***redacted.email@redacted.nope***")
         .version(env!("CARGO_PKG_VERSION"))
         .about("Fuzzy-search the input and return the closest match")
-        .settings(&[AppSettings::SubcommandsNegateReqs, AppSettings::DisableHelpSubcommand, AppSettings::VersionlessSubcommands])
-        .subcommand(SubCommand::with_name("-ce-script")
-            .about("Generate shell script for the `ce` command")
-            .arg(
-                Arg::with_name("shell")
-                    .required(true)
-                    .takes_value(true)
-                    .possible_values(&["bash"])
-            )
+        .settings(&[
+            AppSettings::SubcommandsNegateReqs,
+            AppSettings::DisableHelpSubcommand,
+            AppSettings::VersionlessSubcommands,
+        ])
+        .subcommand(
+            SubCommand::with_name("-ce-script")
+                .about("Generate shell script for the `ce` command")
+                .arg(
+                    Arg::with_name("shell")
+                        .required(true)
+                        .takes_value(true)
+                        .possible_values(&["bash"]),
+                ),
         )
-        .subcommand(SubCommand::with_name("-ce")
-            .about("Fuzzy-searching cd command")
-            .usage("ce <dirs>...")
-            .arg(
-                Arg::with_name("dirs")
-                .help("Sequence of (fuzzy) directory names to cd through")
-                .multiple(true)
-                .required(true)
-            )
+        .subcommand(
+            SubCommand::with_name("-ce")
+                .about("Fuzzy-searching cd command")
+                .usage("ce <dirs>...")
+                .arg(
+                    Arg::with_name("dirs")
+                        .help("Sequence of (fuzzy) directory names to cd through")
+                        .multiple(true)
+                        .required(true),
+                ),
         )
         .arg(
             Arg::with_name("query")
-            .help("The string to search for")
-            .required(true)
+                .help("The string to search for")
+                .required(true),
         )
         .arg(
             Arg::with_name("inputs")
-            .long("--inputs")
-            .short("-i")
-            .help("Lines of input to search")
-            .takes_value(true)
-            .multiple(true)
+                .long("--inputs")
+                .short("-i")
+                .help("Lines of input to search")
+                .takes_value(true)
+                .multiple(true),
         )
         .after_help(
-r#"Fuzzy-search a list of inputs with a query string. 
+            r#"Fuzzy-search a list of inputs with a query string.
 The closest match is written to stdout.
-If no inputs are provided, inputs are read from stdin."#)
+If no inputs are provided, inputs are read from stdin."#,
+        )
 }
 
-
-fn main()
-{
+fn main() {
     let args = cle_app().get_matches();
-    match args.subcommand()
-    {
-        ("-ce-script", Some(args)) =>
-        {
-            match args.value_of("shell").unwrap()
-            {
-                "bash" => output_success(include_str!("scripts/ce.sh")),
-                _ => unreachable!()
-            }
+    match args.subcommand() {
+        ("-ce-script", Some(args)) => match args.value_of("shell").unwrap() {
+            "bash" => output_success(include_str!("scripts/ce.sh")),
+            _ => unreachable!(),
         },
 
-        ("-ce", Some(args)) =>
-        {
+        ("-ce", Some(args)) => {
             use std::path::PathBuf;
 
             let queries = args.values_of("dirs").unwrap();
-            let starting_dir = std::env::current_dir().expect("cle: error: failed to identify current directory");
+            let starting_dir =
+                std::env::current_dir().expect("cle: error: failed to identify current directory");
             let mut working_dir = PathBuf::new();
             working_dir.push(starting_dir);
 
-            for query in queries
-            {
-                if query.starts_with('/')
-                {
+            for query in queries {
+                if query.starts_with('/') {
                     working_dir = PathBuf::new();
                     working_dir.push(query);
-                    continue
+                    continue;
                 }
 
                 let query = query.trim_right_matches('/');
                 let reverse_searching = query.starts_with("..");
 
-                if reverse_searching
-                {
+                if reverse_searching {
                     let (_, query) = query.split_at(2);
 
-                    match query
-                    {
-                        "" => { working_dir.pop(); },
-                        query =>
-                        {
-                            if let Ok(popcount) = query.parse::<u64>()
-                            {
-                                for _ in 0..popcount
-                                {
+                    match query {
+                        "" => {
+                            working_dir.pop();
+                        }
+                        query => {
+                            if let Ok(popcount) = query.parse::<u64>() {
+                                for _ in 0..popcount {
                                     working_dir.pop();
                                 }
-                            }
-                            else
-                            {
+                            } else {
                                 let target = {
-                                    let path_components = working_dir.iter().filter_map(|path| path.to_str());
+                                    let path_components =
+                                        working_dir.iter().filter_map(|path| path.to_str());
                                     let target = close_enough::close_enough(path_components, query);
                                     target.map(|t| t.to_owned())
                                 };
 
-                                match target
-                                {
-                                    Some(ref dir) =>
-                                    {
+                                match target {
+                                    Some(ref dir) => {
                                         let working_dir = &mut working_dir;
-                                        while !working_dir.ends_with(dir)
-                                        {
+                                        while !working_dir.ends_with(dir) {
                                             working_dir.pop();
                                         }
-                                    },
-                                    None => output_failure(format!("ce: No directory name matching '{}': Reached '{}'", query, working_dir.display()))
+                                    }
+                                    None => output_failure(format!(
+                                        "ce: No directory name matching '{}': Reached '{}'",
+                                        query,
+                                        working_dir.display()
+                                    )),
                                 }
                             }
                         }
                     }
-                }
-                else
-                {
+                } else {
                     let dir_contents = std::fs::read_dir(&working_dir).unwrap();
 
-                    let inputs = dir_contents.map(|e|
-                        e.unwrap()).filter_map(|entry|
-                            if entry.file_type().unwrap().is_dir()
-                            {
-                                entry.file_name().into_string().ok()
-                            }
-                            else
-                            {
-                                None
-                            });
+                    let inputs = dir_contents.map(|e| e.unwrap()).filter_map(|entry| {
+                        if entry.file_type().unwrap().is_dir() {
+                            entry.file_name().into_string().ok()
+                        } else {
+                            None
+                        }
+                    });
 
                     let result = close_enough::close_enough(inputs, query);
 
-                    match result
-                    {
+                    match result {
                         Some(dir) => working_dir.push(dir),
-                        None => output_failure(format!("ce: No directory name matching '{}': Reached '{}'", query, working_dir.display()))
+                        None => output_failure(format!(
+                            "ce: No directory name matching '{}': Reached '{}'",
+                            query,
+                            working_dir.display()
+                        )),
                     }
                 }
             }
             output_success(working_dir.as_path().to_str().unwrap());
-        },
+        }
 
-        _ =>
-        {
+        _ => {
             use std::io::Read;
 
             let query = args.value_of("query").unwrap();
             let inputs = args.values_of("inputs");
             let mut stdin = String::new();
 
-            let result: Option<&str> = match inputs
-            {
+            let result: Option<&str> = match inputs {
                 Some(inputs) => close_enough::close_enough(inputs, query),
-                None =>
-                {
-                    std::io::stdin().read_to_string(&mut stdin).expect("cle: error: Failed to read from stdin");
+                None => {
+                    std::io::stdin()
+                        .read_to_string(&mut stdin)
+                        .expect("cle: error: Failed to read from stdin");
                     close_enough::close_enough(stdin.lines(), query)
                 }
             };
 
-            match result
-            {
+            match result {
                 Some(matching) => output_success(matching),
-                None => exit_with_failure()
+                None => exit_with_failure(),
             }
         }
     }
     unreachable!();
 }
 
-
 fn output_success<T>(output: T)
 where
-    T: AsRef<str>
+    T: AsRef<str>,
 {
     print!("{}", output.as_ref());
     std::process::exit(0);
 }
 
-
 fn output_failure<T>(message: T)
 where
-    T: AsRef<str>
+    T: AsRef<str>,
 {
     eprintln!("{}", message.as_ref());
     std::process::exit(1);
 }
 
-
-fn exit_with_failure()
-{
+fn exit_with_failure() {
     std::process::exit(1);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::iter::{Iterator, Peekable};
 
-
 /// Returns the closest match from the given options to the given query.
 ///
 /// This algorithm works by scanning through each option trying to match the beginning of the
@@ -19,27 +18,28 @@ use std::iter::{Iterator, Peekable};
 /// assert_eq!(result, Some(&"one two"));
 /// ```
 pub fn close_enough<I, O, Q>(options: I, query: Q) -> Option<O>
-    where I: IntoIterator<Item=O>, O: AsRef<str>, Q: AsRef<str>
+where
+    I: IntoIterator<Item = O>,
+    O: AsRef<str>,
+    Q: AsRef<str>,
 {
     let mut shortest_answer: Option<O> = None;
 
-    for opt in options
-    {
+    for opt in options {
         let matches = {
             let mut optchars = opt.as_ref().chars().peekable();
             let mut querychars = query.as_ref().chars().peekable();
 
-            while querychars.peek().is_some()
-            {
-                while optchars.peek().is_some() && !same_char(querychars.peek(), optchars.peek())
-                {
+            while querychars.peek().is_some() {
+                while optchars.peek().is_some() && !same_char(querychars.peek(), optchars.peek()) {
                     optchars.next();
                 }
 
-                if optchars.peek().is_none() { break; }
+                if optchars.peek().is_none() {
+                    break;
+                }
 
-                while querychars.peek().is_some() && same_char(querychars.peek(), optchars.peek())
-                {
+                while querychars.peek().is_some() && same_char(querychars.peek(), optchars.peek()) {
                     querychars.next();
                     optchars.next();
                 }
@@ -50,8 +50,7 @@ pub fn close_enough<I, O, Q>(options: I, query: Q) -> Option<O>
             querychars.peek().is_none()
         };
 
-        if matches
-        {
+        if matches {
             shortest_answer = Some(select_shortest(opt, shortest_answer));
         }
     }
@@ -59,38 +58,35 @@ pub fn close_enough<I, O, Q>(options: I, query: Q) -> Option<O>
     shortest_answer
 }
 
-
-fn same_char(a: Option<&char>, b: Option<&char>) -> bool
-{
-    match (a, b)
-    {
+fn same_char(a: Option<&char>, b: Option<&char>) -> bool {
+    match (a, b) {
         (Some(achar), Some(bchar)) => achar.to_lowercase().next() == bchar.to_lowercase().next(),
-        _ => false
+        _ => false,
     }
 }
-
 
 fn select_shortest<T>(proposed: T, previous: Option<T>) -> T
-    where T: AsRef<str>
+where
+    T: AsRef<str>,
 {
-    match previous
-    {
+    match previous {
         None => proposed,
-        Some(prev) => if proposed.as_ref().len() < prev.as_ref().len() { proposed } else { prev }
+        Some(prev) => if proposed.as_ref().len() < prev.as_ref().len() {
+            proposed
+        } else {
+            prev
+        },
     }
 }
 
-
 fn skip_word<I>(chars: &mut Peekable<I>)
-    where I: Iterator<Item=char>
+where
+    I: Iterator<Item = char>,
 {
     let mut has_more = chars.peek().is_some();
-    while has_more
-    {
-        if let Some(c) = chars.peek()
-        {
-            if !c.is_alphanumeric() || c.is_uppercase()
-            {
+    while has_more {
+        if let Some(c) = chars.peek() {
+            if !c.is_alphanumeric() || c.is_uppercase() {
                 break;
             }
         }
@@ -98,90 +94,82 @@ fn skip_word<I>(chars: &mut Peekable<I>)
     }
 }
 
-
 #[cfg(test)]
 mod tests {
 
     use super::*;
 
-    fn test(options: &[&str], query: &str, expected: Option<&str>)
-    {
+    fn test(options: &[&str], query: &str, expected: Option<&str>) {
         assert_eq!(close_enough(options, query), expected.as_ref());
     }
 
     #[test]
-    fn no_options_returns_none()
-    {
+    fn no_options_returns_none() {
         test(&[], "blah", None);
     }
 
     #[test]
-    fn single_option_returned_on_match()
-    {
+    fn single_option_returned_on_match() {
         test(&["only"], "only", Some("only"));
     }
 
     #[test]
-    fn single_option_returns_none_if_not_match()
-    {
+    fn single_option_returns_none_if_not_match() {
         test(&["only"], "different", None);
     }
 
     #[test]
-    fn multiple_options_returns_matching()
-    {
+    fn multiple_options_returns_matching() {
         test(&["one", "two"], "two", Some("two"));
     }
 
     #[test]
-    fn partial_match_returns_answer()
-    {
+    fn partial_match_returns_answer() {
         test(&["only"], "on", Some("only"));
     }
 
     #[test]
-    fn matching_is_case_insensitive()
-    {
+    fn matching_is_case_insensitive() {
         test(&["OnLy"], "only", Some("OnLy"));
     }
 
     #[test]
-    fn multiple_partial_matches_return_shortest_answer()
-    {
+    fn multiple_partial_matches_return_shortest_answer() {
         test(&["item_the_first", "item"], "it", Some("item"));
     }
 
     #[test]
-    fn entire_query_must_be_present_for_match()
-    {
+    fn entire_query_must_be_present_for_match() {
         test(&["item_the_first"], "item_the_fist", None);
     }
 
     #[test]
-    fn can_match_from_beyond_start()
-    {
+    fn can_match_from_beyond_start() {
         test(&["theonlyitem"], "only", Some("theonlyitem"));
     }
 
     #[test]
-    fn failed_match_looks_to_next_word()
-    {
-        test(&["A very_big-longMatch"], "avblm", Some("A very_big-longMatch"));
+    fn failed_match_looks_to_next_word() {
+        test(
+            &["A very_big-longMatch"],
+            "avblm",
+            Some("A very_big-longMatch"),
+        );
     }
 
     #[test]
-    fn failed_match_does_not_look_within_same_word()
-    {
+    fn failed_match_does_not_look_within_same_word() {
         test(&["averybiglongmatch"], "avblm", None);
     }
 
     #[test]
-    fn works_on_useful_collection_types()
-    {
+    fn works_on_useful_collection_types() {
         assert_eq!(close_enough(["a", "thing"].iter(), "thing"), Some(&"thing"));
         assert_eq!(close_enough(&["a", "thing"], "thing"), Some(&"thing"));
         assert_eq!(close_enough(&vec!["a", "thing"], "thing"), Some(&"thing"));
-        assert_eq!(close_enough(&vec!["a".to_owned(), "thing".to_owned()], "thing"), Some(&"thing".to_owned()));
+        assert_eq!(
+            close_enough(&vec!["a".to_owned(), "thing".to_owned()], "thing"),
+            Some(&"thing".to_owned())
+        );
     }
 }
-
